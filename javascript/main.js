fa8f2df0ea71ae2bbf5e248dcfd7f506862f4473
Kacper Rak <rzstunts@gmail.com>
2022-12-09 19:46:35 +0100

    upgrade

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,6 @@
-//random characters in main menu
-
-const randomCharacter = Math.floor(Math.random() * 2) + 1
-
-let imgContainer = document.querySelector('.characters')
-imgContainer.innerHTML = '<img class="character-img" src="../img/characters-for-index/' + randomCharacter + '.png">'  
-
 const bgImage = document.querySelector('.background')
-const windowWidth = window.innerWidth
-const windowHeight = window.innerHeight
-let correctPlus = true
-let correctMinus = true
 
-if(windowWidth / 16 != windowHeight / 9) {
+if(window.innerWidth / 16 != window.innerHeight / 9) {
     bgImage.classList.toggle('smaller-width')
 }
 
