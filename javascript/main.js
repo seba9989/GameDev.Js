1dd725cd4f08d32f94a73e292b0003202c7412f0
Kacper Rak <rzstunts@gmail.com>
2022-12-09 17:45:57 +0100

    main menu 2

--- javascript/main.js
+++ javascript/main.js
@@ -3,5 +3,17 @@
 const randomCharacter = Math.floor(Math.random() * 2) + 1
 
 let imgContainer = document.querySelector('.characters')
-imgContainer.innerHTML = '<img class="character-img" src="../img/characters-for-index/' + randomCharacter + '.png">'    
+imgContainer.innerHTML = '<img class="character-img" src="../img/characters-for-index/' + randomCharacter + '.png">'  
+
+const bgImage = document.querySelector('.background')
+const windowWidth = window.innerWidth
+const windowHeight = window.innerHeight
+let correctPlus = true
+let correctMinus = true
+
+if(windowWidth / 16 != windowHeight / 9) {
+    bgImage.classList.toggle('smaller-width')
+}
+
+
 
