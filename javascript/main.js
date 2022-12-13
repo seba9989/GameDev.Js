45d7bf8d9cefb0001bbe6be018b1efb6e1c891e7
Kacper Rak <rzstunts@gmail.com>
2022-12-13 15:22:39 +0100

    issues corrected

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,38 @@
-// ANIMATIONS IMPORT 
-import * as animate from "./animations.js"
+// ZMIENNE GLOBALNE
+
+
+
+
+
+
 
 document.querySelector('.player-card').classList.add('card-turn')
 
+var attackButtons = document.querySelector('.attack-buttons').innerHTML
 
 function wandButtons() {
 
     gettingWand()
 
-    document.querySelector('.attack-buttons').innerHTML = '<button onclick="returnMainButtons()" class="return-button"><img src="../img/buttons/return-button.png" alt=""></button><button onclick="attackWand()" class="attack-button attack1-button">Attack</button><button onclick="healWand()" class="attack-button attack2-button">Heal</button><button onclick="healWand()" class="attack-button attack2-button">Attack</button>'
+
+    document.querySelector('.attack-buttons').innerHTML = '<button onclick="returnMainButtons()" class="return-button"><img src="../img/buttons/return-button.png" alt=""></button>'
+
+    // ADDING FIRST ATTACK BUTTON
+    const newAttack1 = document.createElement('div')
+    newAttack1.innerText = "Attack"
+    newAttack1.classList.add('attack-button')
+    document.querySelector('.attack-buttons').appendChild(newAttack1)
+    newAttack1.addEventListener('click', attackWand)
+
+    
+
+    // ADDING FIRST ATTACK BUTTON
+    const newAttack2 = document.createElement('div')
+    newAttack2.innerText = "Heal"
+    newAttack2.classList.add('attack-button')
+    document.querySelector('.attack-buttons').appendChild(newAttack2)
+    newAttack2.addEventListener('click', healWand)
+
 
     document.querySelector('.return-button').style.display = 'block'
 }
@@ -25,3 +49,78 @@ function returnMainButtons() {
 
 
 
+// ANIMATIONS 
+
+
+function normalIdle() {
+    let spritePlayer = document.querySelector('.character img')
+
+    spritePlayer.src = "../img/characters/mage/mage-normal.gif"
+}
+
+
+//book animations
+
+function gettingBook() {
+    let spritePlayer = document.querySelector('.character img')
+
+    spritePlayer.src = "../img/characters/mage/getting-book.gif"
+
+    setTimeout(bookIdle, 1100)
+}
+
+function bookIdle() {
+    let spritePlayer = document.querySelector('.character img')
+
+    spritePlayer.src = "../img/characters/mage/book-normal.gif"
+}
+
+function hidingBook() {
+    let spritePlayer = document.querySelector('.character img')
+
+    spritePlayer.src = "../img/characters/mage/hiding-book.gif"
+
+    setTimeout(normalIdle, 1100)
+}
+
+
+
+//wand animations
+
+function gettingWand() {
+    let spritePlayer = document.querySelector('.character img')
+
+    spritePlayer.src = "../img/characters/mage/wand-getting.gif"
+
+    setTimeout(wandIdle, 2000)
+}
+
+function wandIdle() {
+    let spritePlayer = document.querySelector('.character img')
+
+    spritePlayer.src = "../img/characters/mage/wand-idle.gif"
+}
+
+function hidingWand() {
+    let spritePlayer = document.querySelector('.character img')
+
+    spritePlayer.src = "../img/characters/mage/wand-hiding.gif"
+
+    setTimeout(normalIdle, 1000)
+}
+
+function attackWand() {
+    let spritePlayer = document.querySelector('.character img')
+
+    spritePlayer.src = "../img/characters/mage/wand-attack.gif"
+
+    setTimeout(wandIdle, 2000)
+}
+
+function healWand() {
+    let spritePlayer = document.querySelector('.character img')
+
+    spritePlayer.src = "../img/characters/mage/wand-heal.gif"
+
+    setTimeout(wandIdle, 3800)
+}
